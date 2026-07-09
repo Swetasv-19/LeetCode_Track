c625b9845e4c84e7f05023580c962f8f7aaf76fb
Sweta S V <sweta.sv19@gmail.com>
2026-07-09 10:14:56 +0530

    Create 1452-RestaurantGrowth

---
+++ 1452-RestaurantGrowth/1452-RestaurantGrowth.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/9/2026, 10:14:56 AM
+select distinct visited_on,
+        sum(amount) over w as amount,
+        round((sum(amount) over w)/7, 2) as average_amount
+    from customer
+    WINDOW w AS ( 
+            order by visited_on
+            range between interval 6 day PRECEDING and current row
+    )
+    Limit 6, 999
