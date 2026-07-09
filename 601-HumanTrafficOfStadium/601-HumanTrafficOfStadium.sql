87efd1a8d91d65970aed8483a217b3d2b4572a6a
Sweta S V <sweta.sv19@gmail.com>
2026-07-09 10:16:11 +0530

    Create 601-HumanTrafficOfStadium

---
+++ 601-HumanTrafficOfStadium/601-HumanTrafficOfStadium.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/9/2026, 10:16:10 AM
+with q1 as (
+select *, id - row_number() over() as id_diff
+from stadium
+where people > 99
+)
+select id, visit_date, people
+from q1
+where id_diff in (select id_diff from q1 group by id_diff having count(*) > 2)
+order by visit_date
