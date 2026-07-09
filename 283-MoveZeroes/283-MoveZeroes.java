a87957b3ffcf786dc5d8a9a5212920d0a71adbea
Sweta S V <sweta.sv19@gmail.com>
2026-07-09 10:17:24 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 10:17:23 AM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        while (index < nums.length) {
+            nums[index] = 0;
+            index++;
+        }
+    }
+}
