1409a20e46d34ba9bcec2ce5d8912ba74a522a0e
Sweta S V <sweta.sv19@gmail.com>
2026-07-09 10:15:24 +0530

    Create 1034-SubarraysWithKDifferentIntegers

---
+++ 1034-SubarraysWithKDifferentIntegers/1034-SubarraysWithKDifferentIntegers.java
@@ -0,0 +1,39 @@
+// Last updated: 7/9/2026, 10:15:24 AM
+import java.util.*;
+
+class Solution {
+
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    public int atMost(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int left = 0;
+        int count = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            map.put(nums[right],
+                    map.getOrDefault(nums[right], 0) + 1);
+
+            while (map.size() > k) {
+
+                map.put(nums[left],
+                        map.get(nums[left]) - 1);
+
+                if (map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
+                }
+
+                left++;
+            }
+
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+}
