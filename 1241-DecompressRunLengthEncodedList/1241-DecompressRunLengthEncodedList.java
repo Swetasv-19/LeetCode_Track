50724f192431eb66f4ca0302d216e856ce0daf1a
Sweta S V <sweta.sv19@gmail.com>
2026-07-09 10:15:10 +0530

    Create 1241-DecompressRunLengthEncodedList

---
+++ 1241-DecompressRunLengthEncodedList/1241-DecompressRunLengthEncodedList.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 10:15:09 AM
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        ArrayList<Integer> list=new ArrayList<>();
+   
+     for(int i=1;i<nums.length;i++){
+           for(int j=0;j<nums[i-1];j++){
+            list.add(nums[i]);
+           }
+        i++;
+     }
+     int[] arr=new int[list.size()];
+     for(int i=0;i<arr.length;i++){
+        arr[i]=list.get(i);
+     }
+     return arr;
+    }
+}
