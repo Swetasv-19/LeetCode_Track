a752f3d80bd3cb973ae21e090a72f2373717b86f
Sweta S V <sweta.sv19@gmail.com>
2026-07-09 10:16:31 +0530

    Create 496-NextGreaterElementI

---
+++ 496-NextGreaterElementI/496-NextGreaterElementI.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 10:16:31 AM
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] arr = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            arr[i]=-1;
+            boolean f = false;
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    f=true;
+                }
+                if(f&&nums2[j]>nums1[i]){
+                    arr[i]=nums2[j];
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+}
